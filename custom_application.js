ba0030c74833f28e6e581954eba7bfdb758ec683
fm67 <fm67@domU-12-31-39-0C-72-52.compute-1.internal>
2012-06-17 18:50:04 +0000

    complete code

--- custom_application.js
+++ custom_application.js
@@ -1,3 +1,96 @@
+
+var CountdownTimer = {
+  endTime:null,
+  timerId:null,
+  minutes:0,
+  maxWidth:0,
+  maxTime:(25 * 60 * 1000),
+  isBreak:false,
+
+  normalColors:["#e47a80", "#eb363b"],
+  breakColors: ["#97c751", "#759f58"],
+
+  start: function(minutes, isBreak) {
+    if (this.maxWidth == 0)
+      this.maxWidth = $('#timer-bar').width();
+
+    if (this.timerId)
+      this.cancel();
+
+    this.minutes = minutes;
+    this.isBreak = isBreak;
+
+    this.setupTimer();
+    this.flashBar();
+  },
+
+  setupTimer:function() {
+    $('#timer-finished').fadeOut();
+
+    var date     = new Date();
+    this.endTime = date.getTime() + (this.minutes * 60 * 1000);
+    this.timerId = setInterval(this.tick, 1000);
+
+    this.setBarToTime(this.minutes * 60 * 1000);
+    $('title').text("Started " + 
+      (this.isBreak ? this.minutes + " minute break." : "pomodoro."));
+  },
+
+  flashBar:function() {
+    var lightColor, darkColor;
+    if (this.isBreak)
+    {
+      lightColor = this.breakColors[0];
+      darkColor  = this.breakColors[1];
+    } else {
+      lightColor = this.normalColors[0];
+      darkColor  = this.normalColors[1];
+    }
+    $('#timer-bar').animate({backgroundColor:lightColor})
+      .animate({backgroundColor:darkColor})
+      .css('background-color', darkColor);
+  },
+
+  setBarToTime:function(time) {
+    $('#timer-bar').animate({width:(time/this.maxTime) * this.maxWidth});
+  },
+
+  cancel:function() {
+    clearInterval(this.timerId);
+  },
+
+  tick:function() {
+     CountdownTimer.tock();
+  },
+
+  tock:function() {
+    var date = new Date();
+    var remainingTime = this.endTime - date.getTime();
+    if (remainingTime <= 0) {
+      this.cancel();
+      this.setBarToTime(0);
+      this.showCompletedInLog();
+      this.showCompletedMessage();
+    } else {
+      this.setBarToTime(remainingTime);
+    }
+  },
+
+  showCompletedInLog:function() {
+    var logClass = "timer-log-" + 
+      this.minutes + (this.isBreak ? "-break" : "");
+    $('#timer-log').append('<div class="' + logClass + '"></div>');
+  },
+
+  showCompletedMessage:function() {
+    if ($('#timer-finished').length == 0)
+      $('#timer').append('<div id="timer-finished"></div>');
+    var message = "Finished " + 
+      (this.isBreak ? this.minutes + " minute break." : "one pomodoro.");
+    $('title').text(message);
+    $('#timer-finished').text(message).fadeIn(1000);
+  },
+};
 jQuery.fn.extend({
 
   taskStates:['task-empty', 'task-x', 'task-apostrophe', 'task-dash'],
